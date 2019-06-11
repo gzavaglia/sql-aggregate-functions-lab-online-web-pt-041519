31521e5390a4582bb85b99052069cf34ca136ccf
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-11 18:06:14 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -9,7 +9,7 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
-  SELECT AVG(net_worth) AS average_net_worth FROM cats;
+  "SELECT AVG(net_worth) FROM cats;"
 end
 
 def total_tardies_for_all_students
